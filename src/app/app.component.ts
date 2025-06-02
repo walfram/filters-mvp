bb32c630d8e885a3149d7c503c9688e0dae1b101
jevgeni.kalinin <jevgeni.kalinin@gmail.com>
2025-06-02 08:54:17 +0300

    Add initial HomeComponent with routing, styling, and dependencies
    - Added `HomeComponent` with template, style, and spec files.
    - Updated `package.json` and `package-lock.json` to include `@angular/material`, `@angular/cdk`, and `zod`.
    - Adjusted global styles and imported Roboto font and Material icons.
    - Replaced placeholder content in `app.component.html` with Material Toolbar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
