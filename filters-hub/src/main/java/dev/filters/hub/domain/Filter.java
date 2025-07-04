7c7d68bc65bbd006b55dd292b49491cc03e18130
jevgeni.kalinin <jevgeni.kalinin@gmail.com>
2025-07-04 12:14:32 +0300

    added id to Filter.java

--- filters-hub/src/main/java/dev/filters/hub/domain/Filter.java
+++ filters-hub/src/main/java/dev/filters/hub/domain/Filter.java
@@ -3,8 +3,10 @@ package dev.filters.hub.domain;
 import jakarta.validation.Valid;
 
 import java.util.List;
+import java.util.UUID;
 
 public record Filter(
+    UUID id,
     String name,
     boolean active,
     List<@Valid Condition> conditions
