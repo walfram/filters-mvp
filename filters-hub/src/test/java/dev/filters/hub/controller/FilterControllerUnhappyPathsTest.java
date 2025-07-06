cc19184d155af3925feb01a254704248d30fc217
jevgeni.kalinin <jevgeni.kalinin@gmail.com>
2025-07-06 09:26:19 +0300

    fixed FilterControllerUnhappyPathsTest

--- filters-hub/src/test/java/dev/filters/hub/controller/FilterControllerUnhappyPathsTest.java
+++ filters-hub/src/test/java/dev/filters/hub/controller/FilterControllerUnhappyPathsTest.java
@@ -1,10 +1,12 @@
 package dev.filters.hub.controller;
 
+import dev.filters.hub.service.FilterService;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -15,6 +17,9 @@ public class FilterControllerUnhappyPathsTest {
 
   @Autowired
   private MockMvc mockMvc;
+
+  @MockitoBean
+  private FilterService filterService;
 
   @ParameterizedTest
   @ValueSource(strings = {
