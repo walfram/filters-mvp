2190983fb3dfb5c1df9e2475a334694f158b3dc8
jevgeni.kalinin <jevgeni.kalinin@gmail.com>
2025-09-24 22:06:28 +0300

    refactored FilterForm to enhance type safety with strongly typed form groups, updated styles with improved layout and error indicators, and integrated Material Datepicker for date criteria input

--- filters-next-gen/src/app/features/filters/filter-form/filter-form.ts
+++ filters-next-gen/src/app/features/filters/filter-form/filter-form.ts
@@ -1,7 +1,7 @@
 import {Component, EventEmitter, inject, Input, OnInit, Output} from '@angular/core';
 import {Filter} from '../types/filter';
-import {FormArray, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
-import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {FormArray, FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormField, MatHint, MatLabel} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {MatButton, MatMiniFabButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
@@ -9,6 +9,42 @@ import {AmountCriterion, Criterion, DateCriterion, TitleCriterion} from '../type
 import {v4} from 'uuid';
 import {MatOption, MatSelect} from '@angular/material/select';
 import {MatCheckbox} from '@angular/material/checkbox';
+import {NgClass} from '@angular/common';
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
+
+interface AmountCriterionForm {
+  id: FormControl<string>;
+  type: FormControl<'number'>;
+  operator: FormControl<'eq' | 'gt' | 'gte' | 'lt' | 'lte'>;
+  value: FormControl<number>;
+}
+
+interface TitleCriterionForm {
+  id: FormControl<string>;
+  type: FormControl<'string'>;
+  operator: FormControl<'eq' | 'contains' | 'startsWith' | 'endsWith'>;
+  value: FormControl<string>;
+  caseSensitive: FormControl<boolean>;
+}
+
+interface DateCriterionForm {
+  id: FormControl<string>;
+  type: FormControl<'date'>;
+  operator: FormControl<'before' | 'after' | 'equals'>;
+  value: FormControl<Date>;
+}
+
+type CriterionFormGroup =
+  FormGroup<AmountCriterionForm> |
+  FormGroup<TitleCriterionForm> |
+  FormGroup<DateCriterionForm>;
+
+interface FilterFormGroup {
+  id: FormControl<string>;
+  name: FormControl<string>;
+  criteria: FormArray<CriterionFormGroup>;
+}
 
 @Component({
   selector: 'app-filter-form',
@@ -22,10 +58,16 @@ import {MatCheckbox} from '@angular/material/checkbox';
     MatIcon,
     MatSelect,
     MatOption,
-    MatCheckbox
+    MatCheckbox,
+    NgClass,
+    MatHint,
+    MatDatepickerToggle,
+    MatDatepicker,
+    MatDatepickerInput
   ],
   templateUrl: './filter-form.html',
-  styleUrl: './filter-form.css'
+  styleUrl: './filter-form.css',
+  providers: [provideNativeDateAdapter()]
 })
 export class FilterForm implements OnInit {
 
@@ -34,22 +76,25 @@ export class FilterForm implements OnInit {
 
   private readonly formBuilder = inject(NonNullableFormBuilder);
 
-  protected form!: FormGroup;
+  protected form!: FormGroup<FilterFormGroup>;
 
   get criteria() {
-    return this.form.get('criteria') as FormArray;
+    return this.form.controls.criteria;
   }
 
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
+    const groups: CriterionFormGroup[]
+      = this.filter.criteria.map(c => this.createCriterionGroup(c));
+
+    this.form = this.formBuilder.group<FilterFormGroup>({
+      id: this.formBuilder.control(this.filter.id, [Validators.required]),
       name: this.formBuilder.control(this.filter.name, [Validators.required, Validators.minLength(3), Validators.maxLength(100)]),
-      criteria: this.formBuilder.array(
-        this.filter.criteria.map(c => this.createCriterionGroup(c)
-        ), [Validators.required])
+      // new FormArray and not this.formBuilder.array 'cause TypeScript was going nuts
+      criteria: new FormArray<CriterionFormGroup>(groups, [Validators.required])
     });
   }
 
-  private createCriterionGroup(criterion: Criterion): FormGroup {
+  private createCriterionGroup(criterion: Criterion): CriterionFormGroup {
     switch (criterion.type) {
       case 'number':
         return this.createAmountCriterionGroup(criterion);
@@ -62,40 +107,42 @@ export class FilterForm implements OnInit {
     }
   }
 
-  private createAmountCriterionGroup(criterion: AmountCriterion): FormGroup {
-    return this.formBuilder.group({
-      id: [criterion.id],
-      type: ['number', Validators.required],
-      operator: [criterion.operator, Validators.required],
-      value: [criterion.value, Validators.required],
+  private createAmountCriterionGroup(criterion: AmountCriterion): FormGroup<AmountCriterionForm> {
+    return this.formBuilder.group<AmountCriterionForm>({
+      id: this.formBuilder.control(criterion.id),
+      type: this.formBuilder.control('number', [Validators.required]),
+      operator: this.formBuilder.control(criterion.operator, [Validators.required]),
+      value: this.formBuilder.control(criterion.value, [Validators.required])
     });
   }
 
-  private createTitleCriterionGroup(criterion: TitleCriterion): FormGroup {
-    return this.formBuilder.group({
-      id: [criterion.id],
-      type: ['string', Validators.required],
-      operator: [criterion.operator, Validators.required],
-      value: [criterion.value, Validators.required],
-      caseSensitive: [criterion.caseSensitive],
+  private createTitleCriterionGroup(criterion: TitleCriterion): FormGroup<TitleCriterionForm> {
+    return this.formBuilder.group<TitleCriterionForm>({
+      id: this.formBuilder.control(criterion.id),
+      type: this.formBuilder.control('string', [Validators.required]),
+      operator: this.formBuilder.control(criterion.operator, [Validators.required]),
+      value: this.formBuilder.control(criterion.value, [Validators.required]),
+      caseSensitive: this.formBuilder.control(criterion.caseSensitive)
     });
   }
 
-  private createDateCriterionGroup(criterion: DateCriterion): FormGroup {
-    return this.formBuilder.group({
-      id: [criterion.id],
-      type: ['date', Validators.required],
-      operator: [criterion.operator, Validators.required],
-      value: [criterion.value, Validators.required],
+  private createDateCriterionGroup(criterion: DateCriterion): FormGroup<DateCriterionForm> {
+    return this.formBuilder.group<DateCriterionForm>({
+      id: this.formBuilder.control(criterion.id),
+      type: this.formBuilder.control('date', [Validators.required]),
+      operator: this.formBuilder.control(criterion.operator, [Validators.required]),
+      value: this.formBuilder.control(criterion.value, [Validators.required])
     });
   }
 
   onSubmit() {
+    console.log('raw value', this.form.getRawValue());
+    console.log('value', this.form.value);
+
     if (this.form.valid) {
       this.submit.emit({
         ...this.form.getRawValue(),
         id: this.filter.id
-        // criteria: [] // TODO fixme :)
       });
     } else {
       console.log('invalid form');
@@ -112,7 +159,7 @@ export class FilterForm implements OnInit {
     console.log('control', control);
     // const type = control.get('type')?.value;
     console.log('type', type);
-    const id = control.get('id')?.value as string;
+    const id = control.controls.id.value;
     console.log('id', id);
 
     switch (type) {
@@ -165,4 +212,6 @@ export class FilterForm implements OnInit {
       value: new Date()
     }
   }
+
+  protected readonly String = String;
 }
