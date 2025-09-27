c603fce50a27edee204f63aa053fd448b2b1460f
jevgeni.kalinin <jevgeni.kalinin@gmail.com>
2025-09-27 21:49:46 +0300

    cleaned up imports in app.config.ts by removing unused Material modules and optimizing formatting for consistency

--- filters-next-gen/src/app/app.config.ts
+++ filters-next-gen/src/app/app.config.ts
@@ -1,9 +1,9 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import {DateFnsAdapter, MAT_DATE_FNS_FORMATS, provideDateFnsAdapter} from '@angular/material-date-fns-adapter';
-import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import {routes} from './app.routes';
+import {provideDateFnsAdapter} from '@angular/material-date-fns-adapter';
+import {MAT_DATE_LOCALE} from '@angular/material/core';
 import {et} from 'date-fns/locale/et';
 
 export const appConfig: ApplicationConfig = {
